e1d67c4af98766f1faf2f464cd2f2e3c08389250
Russell Hueske <hueske.russ690@gmail.com>
2016-03-26 22:15:11 -0500

    Page loads latest release dynamically. Or at least it should.

--- js/app.js
+++ js/app.js
@@ -21,6 +21,8 @@ app.controller('cmdppController', ['$scope', '$http', '$rootScope', 'angularLoad
         breakLine: false
     });
 
+    var cmd;
+
     $http.get('https://api.github.com/repos/cmdPP/core/releases').then(function(res) {
         var assets = res.data[0].assets;
         var bundleURL = "";
@@ -29,63 +31,118 @@ app.controller('cmdppController', ['$scope', '$http', '$rootScope', 'angularLoad
                 bundleURL = asset.browser_download_url;
             }
         }
-
-    });
-
-    var cmd = new CMD({
-        debug: true,
-        funcs: {
-            respond: function() {
-                console.log(arguments);
-                $scope.$broadcast('terminal-output', {
-                    output: true,
-                    text: arguments,
-                    breakLine: true
-                });
-            },
-            save: function(cmdData) {
-                if (typeof Storage !== "undefined") {
-                    for (var i in cmdData) {
-                        localStorage.setItem(i, JSON.stringify(cmdData[i]));
+        return angularLoad.loadScript(bundleURL);
+    }).then(function() {
+        var cmd = new CMD({
+            debug: true,
+            funcs: {
+                respond: function() {
+                    console.log(arguments);
+                    $scope.$broadcast('terminal-output', {
+                        output: true,
+                        text: arguments,
+                        breakLine: true
+                    });
+                },
+                save: function(cmdData) {
+                    if (typeof Storage !== "undefined") {
+                        for (var i in cmdData) {
+                            localStorage.setItem(i, JSON.stringify(cmdData[i]));
+                        }
                     }
+                },
+                load: function() {
+                    var storedDataNames = ['data', 'money', 'increment', 'autoIncrement', 'unlocked'];
+                    var loadObj = {};
+                    for (var i = 0; i < storedDataNames.length; i++) {
+                        var dataName = storedDataNames[i];
+                        loadObj[dataName] = JSON.parse(localStorage.getItem(dataName));
+                    }
+                    return loadObj;
+                },
+                update: function(cmdObj) {
+                    $scope.formattedBytes = cmdObj.formatBytes();
+                    $scope.money = cmdObj.money;
+                    $scope.storage = cmdObj.formatter(cmdObj.storages[cmdObj.storage].capacity);
+                    $scope.$apply();
                 }
-            },
-            load: function() {
-                var storedDataNames = ['data', 'money', 'increment', 'autoIncrement', 'unlocked'];
-                var loadObj = {};
-                for (var i = 0; i < storedDataNames.length; i++) {
-                    var dataName = storedDataNames[i];
-                    loadObj[dataName] = JSON.parse(localStorage.getItem(dataName));
-                }
-                return loadObj;
-            },
-            update: function(cmdObj) {
-                $scope.formattedBytes = cmdObj.formatBytes();
-                $scope.money = cmdObj.money;
-                $scope.storage = cmdObj.formatter(cmdObj.storages[cmdObj.storage].capacity);
-                $scope.$apply();
             }
-        }
-    });
-
-    $scope.version = 'v'+cmd.version;
-
-    $scope.$on('terminal-input', function(e, consoleInput) {
-        cmd.command(consoleInput[0].command);
-    });
-
-    setTimeout(function() {
-        $scope.$broadcast('terminal-output', {
-            output: true,
-            text: [
-                'Welcome to CMD++',
-                'This is a demo.',
-                'Have fun!\n\n',
-                'Please type "help" to open a list of commands.'
-            ]
         });
-        $scope.$apply();
-    }, 100);
+
+        $scope.version = 'v'+cmd.version;
+
+        $scope.$on('terminal-input', function(e, consoleInput) {
+            cmd.command(consoleInput[0].command);
+        });
+
+        setTimeout(function() {
+            $scope.$broadcast('terminal-output', {
+                output: true,
+                text: [
+                    'Welcome to CMD++',
+                    'This is a demo.',
+                    'Have fun!\n\n',
+                    'Please type "help" to open a list of commands.'
+                ]
+            });
+            $scope.$apply();
+        }, 100);
+    });
+
+    // var cmd = new CMD({
+    //     debug: true,
+    //     funcs: {
+    //         respond: function() {
+    //             console.log(arguments);
+    //             $scope.$broadcast('terminal-output', {
+    //                 output: true,
+    //                 text: arguments,
+    //                 breakLine: true
+    //             });
+    //         },
+    //         save: function(cmdData) {
+    //             if (typeof Storage !== "undefined") {
+    //                 for (var i in cmdData) {
+    //                     localStorage.setItem(i, JSON.stringify(cmdData[i]));
+    //                 }
+    //             }
+    //         },
+    //         load: function() {
+    //             var storedDataNames = ['data', 'money', 'increment', 'autoIncrement', 'unlocked'];
+    //             var loadObj = {};
+    //             for (var i = 0; i < storedDataNames.length; i++) {
+    //                 var dataName = storedDataNames[i];
+    //                 loadObj[dataName] = JSON.parse(localStorage.getItem(dataName));
+    //             }
+    //             return loadObj;
+    //         },
+    //         update: function(cmdObj) {
+    //             $scope.formattedBytes = cmdObj.formatBytes();
+    //             $scope.money = cmdObj.money;
+    //             $scope.storage = cmdObj.formatter(cmdObj.storages[cmdObj.storage].capacity);
+    //             $scope.$apply();
+    //         }
+    //     }
+    // });
+
+    // $scope.version = 'v'+cmd.version;
+    //
+    // $scope.$on('terminal-input', function(e, consoleInput) {
+    //     cmd.command(consoleInput[0].command);
+    // });
+    //
+    // setTimeout(function() {
+    //     $scope.$broadcast('terminal-output', {
+    //         output: true,
+    //         text: [
+    //             'Welcome to CMD++',
+    //             'This is a demo.',
+    //             'Have fun!\n\n',
+    //             'Please type "help" to open a list of commands.'
+    //         ]
+    //     });
+    //     $scope.$apply();
+    // }, 100);
 
     console.log($rootScope);
 }]);
