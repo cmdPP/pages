70d513d51cf9b3c314945214129ffe88c02948b8
Russell Hueske <hueske.russ690@gmail.com>
2016-03-31 16:52:34 -0500

    ugh

--- js/lib/vtortola.ng-terminal.js
+++ js/lib/vtortola.ng-terminal.js
@@ -276,16 +276,16 @@
             console.log('New autocomplete:', newVal);
             console.log('Old val:', oldVal);
         }
-    })
+    });
 }])
 
 .directive('terminal', function ($document) {
     return {
         restrict: 'E',
         controller: 'terminalController',
-        scope: {
-            autocomplete: '='
-        },
+        // scope: {
+        //     autocomplete: '='
+        // },
         transclude: true,
         replace:true,
         // template: "<section class='terminal' ng-paste='handlePaste($event)'><div class='terminal-viewport'><div class='terminal-results'></div><span class='terminal-prompt' ng-show='showPrompt'>{{prompt.text}}</span><span class='terminal-input'>{{commandLine}}</span><span class='terminal-cursor'>_</span><input type='text' ng-model='commandLine' class='terminal-target'/></div><div ng-transclude></div></section>",
